6141b44bd6ad5cf293f591b738f96d9f35fa6503
PatilGitH <manavpatil.kop@gmail.com>
2024-12-16 00:46:58 +0530

    additional cade done

--- GitDemo/src/com/velocity/Example.java
+++ GitDemo/src/com/velocity/Example.java
@@ -4,8 +4,8 @@ public class Example {
 
 	public static void main(String[] args) {
 
-		for(int i=0; i<5; i++) {
-			System.out.println();
+		for(int i=0; i<3; i++) {
+			System.out.println("github");
 		}
 
 	}
